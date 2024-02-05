1847d98c5ce0883988d6810ac6b2762e8960c3b7
HitandFeyd <cchavez572@cnm.edu>
2024-02-05 06:20:17 -0700

    Add project files.

---
+++ CChavezQ1/GravForceCalc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChavezQ1
+{
+    class GravForceCalc
+    {
+        //  Variable Declaration 
+        private double force;   //force:    Output gravitaional force in newtons
+        private const double GRAVCONST = 0.0000000000667404; //
+        private double calulatedConstant;
+        private double objMass1;    // M1:  mass of the fiirst object in kiligrams
+        private double objMass2;    // M2:  mass o the second object in kilograms
+        private double objDist;     // R2:  distance between objects in meeters
+
+        // Constructor
+        public GravForceCalc()
+        {
+
+        }
+
+        public static bool DoAnother()
+        {
+            Console.WriteLine("Do you want do another calulation? (y/n): ");
+            string? answer = Console.ReadLine();
+            if (string.IsNullOrEmpty(answer))
+            {
+                return false;
+            }
+            return answer.ToLower()[0] == 'y';
+        }
+
+
+    }
+}
---
+++ CChavezQ1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace CChavezQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                //  In main start a do another loop.
+                //  Instantiate your class.
+                GravForceCalc forceCalc = new GravForceCalc();
+                //  Ask the user for the names and masses of the objects and their distance apart.
+                //  Display the ToString() to the user
+                //  then ask the user if they want to do another.
+            } while (GravForceCalc.DoAnother());
+        }
+    }
+}
