266169c0bd2c35d83bfca9311409056f6fb3d3b9
HitandFeyd <cchavez572@cnm.edu>
2024-02-05 08:31:12 -0700

    working commit

--- CChavezQ1/GravForceCalc.cs
+++ CChavezQ1/GravForceCalc.cs
@@ -10,11 +10,13 @@ namespace CChavezQ1
     {
         //  Variable Declaration 
         private double force;   //force:    Output gravitaional force in newtons
-        private const double GRAVCONST = 0.0000000000667404; //
-        private double calulatedConstant;
+        private const double GRAVCONST = 0.0000000000667404; // 6.67408 x 10-11 
+        private string objName1;    // Name of object 1
         private double objMass1;    // M1:  mass of the fiirst object in kiligrams
-        private double objMass2;    // M2:  mass o the second object in kilograms
+        private string objName2;    // Name of object 2
+        private double objMass2;    // M2:  mass of the second object in kilograms
         private double objDist;     // R2:  distance between objects in meeters
+        
 
         // Constructor
         public GravForceCalc()
@@ -22,6 +24,60 @@ namespace CChavezQ1
 
         }
 
+        // Setters
+        public void SetObjName1(string _objName1)
+        {
+            objName1 = _objName1;
+        }
+
+        public void SetObjMass1(double _objMass1)
+        {
+            objMass1 = _objMass1;
+        }
+        public void SetObjName2(string _objName2)
+        {
+            objName2 = _objName2;
+        }
+
+        public void SetObjMass2(double _objMass2)
+        {
+            objMass2 = _objMass2;
+        }
+        public void SetObjDist(double _objDist)
+        {
+            objDist= _objDist;
+        }
+
+
+        // Getters
+        public string GetObjName1() 
+        { 
+            return objName1;
+        }
+        public string GetObjName2()
+        {
+            return objName2;
+        }
+        public double GetObjMass1()
+        {
+            return objMass1;
+        }
+        public double GetObjMass2()
+        {
+            return objMass2;
+        }
+        public double GetObjDist()
+        {
+            return objDist;
+        }
+
+        //  Private Calulate methods
+
+        private void CalulateConstant()
+        {
+            force = GRAVCONST * ((objMass1 * objMass2) / objDist);
+        }
+        //  Do another method
         public static bool DoAnother()
         {
             Console.WriteLine("Do you want do another calulation? (y/n): ");
@@ -31,6 +87,11 @@ namespace CChavezQ1
                 return false;
             }
             return answer.ToLower()[0] == 'y';
+        }
+
+        public override string ToString()
+        {
+            return "The gravitational force between " + objName1 + " and " + objName2 + " when they are " + objDist+ " meters apart is : " + force + " in Newtons";
         }
 
 
--- CChavezQ1/Program.cs
+++ CChavezQ1/Program.cs
@@ -6,15 +6,61 @@ namespace CChavezQ1
     {
         static void Main(string[] args)
         {
+            //  In main start a do another loop.
             do
             {
-                //  In main start a do another loop.
+
+                DisplayHeader();
                 //  Instantiate your class.
                 GravForceCalc forceCalc = new GravForceCalc();
                 //  Ask the user for the names and masses of the objects and their distance apart.
-                //  Display the ToString() to the user
+                // variable setup 
+                string obj1NameBuffer;
+                string obj2NameBuffer;
+                string objMassBuffer;
+                string objDistanceBuffer;
+                try
+                {
+                    Console.WriteLine("\nWhat is the name of the first stellar object in kilograms?\n");
+                    obj1NameBuffer = Console.ReadLine();
+                    forceCalc.SetObjName1(obj1NameBuffer);
+
+                    Console.WriteLine($"\nWhat is the mass of the {obj1NameBuffer}?\n");
+                    objMassBuffer = Console.ReadLine();
+                    forceCalc.SetObjMass1(double.Parse(objMassBuffer));
+
+                    Console.WriteLine("\nWhat is the name of the second stellar object?\n");
+                    obj2NameBuffer = Console.ReadLine();
+                    forceCalc.SetObjName2(obj2NameBuffer);
+
+                    Console.WriteLine($"\nWhat is the mass of the {obj2NameBuffer} in kilograms?\n");
+                    objMassBuffer = Console.ReadLine();
+                    forceCalc.SetObjMass2(double.Parse(objMassBuffer));
+
+                    Console.WriteLine($"\nWhat is the distance between {obj1NameBuffer}, and {obj2NameBuffer} in meters?\n");
+                    objDistanceBuffer = Console.ReadLine();
+                    forceCalc.SetObjDist(double.Parse(objDistanceBuffer));
+                    //  Display the ToString() to the user
+                    Console.WriteLine(forceCalc.ToString());
+
+                }
+                catch 
+                {
+                    Console.WriteLine($"An error occured!");
+                }
                 //  then ask the user if they want to do another.
             } while (GravForceCalc.DoAnother());
+            Goodbye();
+        }
+
+        // basic main methods could have been build into the calulator class but this stuff isnt the job of a calulation class so i left it in main. 
+        static void DisplayHeader()
+        {
+            Console.WriteLine(".NET I/C SHARP (U01) Class Header\nProgramed by: Colby Chavez\nEmail:cchavez572@cnm.edu\nProgram goal: CChavezQ1 is the demonstation of a class that will calulate the G force between two stellar objects. \n\n");
+        }
+        static void Goodbye()
+        {
+            Console.WriteLine("Thank you for grading CChavezQ1. Have a nice day. \n\n");
         }
     }
 }
